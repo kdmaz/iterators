48789d466e9e4c2b68e454a900162c52c461e0f1
kyle.mazeika <kyle.mazeika@beckshybrids.com>
2022-05-22 12:31:00 -0400

    complete map function

--- src/map.rs
+++ src/map.rs
@@ -1,20 +1,65 @@
-pub struct Map<T, F> {
-    iter: T,
+pub struct Map<I, F, U>
+where
+    I: Iterator,
+    F: FnMut(I::Item) -> U,
+{
+    iter: I,
     f: F,
 }
 
-impl<T, F> Map<T, F> {
-    fn new(iter: T, f: F) -> Map<T, F> {
+impl<I, F, U> Map<I, F, U>
+where
+    I: Iterator,
+    F: FnMut(I::Item) -> U,
+{
+    fn new(iter: I, f: F) -> Map<I, F, U>
+    where
+        I: Iterator,
+        F: FnMut(I::Item) -> U,
+    {
         Map { iter, f }
+    }
+}
+
+pub trait MapExt: Iterator + Sized {
+    fn map2<F, U>(self, f: F) -> Map<Self, F, U>
+    where
+        F: FnMut(Self::Item) -> U;
+}
+
+impl<I> MapExt for I
+where
+    I: Iterator,
+{
+    fn map2<F, U>(self, f: F) -> Map<Self, F, U>
+    where
+        F: FnMut(Self::Item) -> U,
+    {
+        Map::new(self, f)
+    }
+}
+
+impl<I, F, U> Iterator for Map<I, F, U>
+where
+    I: Iterator,
+    F: FnMut(I::Item) -> U,
+{
+    type Item = U;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.iter.next()?;
+        Some((self.f)(item))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::map::MapExt;
+
     #[test]
     fn multiply() {
         let nums = [1, 2, 3];
-        let multiplied = nums.into_iter().map(|num| num * num).collect::<Vec<i32>>();
+        let multiplied = nums.into_iter().map2(|num| num * num).collect::<Vec<i32>>();
         assert_eq!(multiplied, [1, 4, 9]);
     }
 }
